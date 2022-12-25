145dd7dcb73ccdd54e8c0dd7406cb459bcbc8c61
kanthamas <kanthamas.iamrhod@gmail.com>
2022-12-25 22:29:25 +0700

    chore: add jsd3g6 repo

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -17,13 +17,14 @@ const Portfolio = () => {
 						<div className='right-card'>
 							<div className='right-card-title'>
 								<h2>EliteMove</h2>
+								{/* https://github.com/JSD3G6 */}
 							</div>
 							<div className='right-card-content'>
 								<p>Sports Tracking Web Application</p>
 								<p>MERN stack</p>
 								<p>Frond-end: using React.js for UI, React-router-dom for routing, and Axios for HTTP client request handling</p>
 								<p>Back-end: using Node.js and Express.js for managing API and token-based authentication, and Mongoose for dealing with MongoDB database</p>
-								
+
 								<ul>Pages:</ul>
 								<li>Landing Page</li>
 								<li>Register Page</li>
