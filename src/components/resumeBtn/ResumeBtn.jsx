be4569765d10402a16c8c9e9215de6a403aace80
kanthamas <kanthamas.iamrhod@gmail.com>
2023-01-14 15:33:35 +0700

    update url on resume pdf

--- src/components/resumeBtn/ResumeBtn.jsx
+++ src/components/resumeBtn/ResumeBtn.jsx
@@ -2,11 +2,16 @@ import resume from "../../assets/Kanthamas_Iamrhod_Resume_2023.pdf";
 import "./resumeBtn2.scss";
 
 const ResumeBtn = () => {
-	return (
-		<a href={resume} download={`Kanthamas_Iamrhod_Resume.pdf`}>
-			<button className='btn-resume'></button>
-		</a>
-	);
+  return (
+    <a
+      href={resume}
+      download={`Kanthamas_Iamrhod_Resume.pdf`}
+      rel='noopener noreferrer'
+      target='_blank'
+    >
+      <button className='btn-resume'></button>
+    </a>
+  );
 };
 
 export default ResumeBtn;
